6b106d52dbd8314bd746727554a65889742ca7a7
Harshit Verma <harshit.20214269@mnnit.ac.in>
2023-07-13 08:17:31 +0530

    Complete Code of FIRSTDOC (My first MERN stack Project)

---
+++ client/src/pages/Login.js
@@ -0,0 +1,137 @@
+// import React from 'react'
+// import {Form,Input,message} from 'antd'
+// import { Link, useNavigate } from "react-router-dom";
+// // import "../styles/RegisterStyles.css";
+// import { showLoading, hideLoading } from "../redux/features/alertSlice";
+// import "../styles/Login.css";
+
+// import axios from 'axios'
+// import { useDispatch } from "react-redux";
+
+// const Login = () => {
+//     const navigate = useNavigate();
+//     const dispatch = useDispatch();
+//     //form handler
+//     const onFinishHandler = async (values) => {
+//       try {
+//         dispatch(showLoading());
+//         const res = await axios.post("/api/v1/user/login", values);
+       
+//         dispatch(hideLoading());
+//         if (res.data.success) {
+//           localStorage.setItem("token", res.data.token);
+//           message.success("Login Successfully");
+//           navigate("/");
+//         } else {
+//           message.error(res.data.message);
+//         }
+//       } catch (error) {
+//         dispatch(hideLoading());
+//         console.log(error);
+//         message.error("something went wrong");
+//       }
+//     };
+    
+//     return (
+//       <div className="form-container ">
+//          <div class="outer_box">
+//         <div class="inner_box">
+//             <header class="sign_up_header">
+//                 <h1>Signup</h1>
+//                 <p>It will just take 10 sec</p>
+                
+//             </header>
+//         <main class="sign_up_body">
+//         <Form
+//           layout="vertical"
+//           onFinish={onFinishHandler}
+//           className="register-form"
+//         >
+//           {/* <h3 className="text-center">Login From</h3> */}
+  
+//           <Form.Item label="Email" name="email">
+//             <Input type="email" required />
+//           </Form.Item>
+//           <Form.Item label="Password" name="password">
+//             <Input type="password" required />
+//           </Form.Item>
+//           </Form>
+//           </main>
+//           <footer class="signup_footer">
+//           <Link to="/register" className="m-2">
+//             Not a user Register here
+//           </Link>
+//           <button className="btn btn-primary" type="submit">
+//             Login
+//           </button>
+//           </footer>
+        
+       
+//         </div>
+//         <div class="circle c1"></div>
+//        <div class="circle c2"></div>
+//       </div>
+//       </div>
+//     );
+//   };
+  
+//   export default Login;
+  
+import React from "react";
+import "../styles/RegisterStyles.css";
+import { Form, Input, message } from "antd";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  //form handler
+  const onfinishHandler = async (values) => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post("/api/v1/user/login", values);
+      window.location.reload();
+      dispatch(hideLoading());
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+      message.error("something went wrong");
+    }
+  };
+  return (
+    <div className="form-container ">
+      <Form
+        layout="vertical"
+        onFinish={onfinishHandler}
+        className="register-form"
+      >
+        <h3 className="text-center">Login From</h3>
+
+        <Form.Item label="Email" name="email">
+          <Input type="email" required />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" required />
+        </Form.Item>
+        <Link to="/register" className="m-2">
+          Not a user Register here
+        </Link>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
